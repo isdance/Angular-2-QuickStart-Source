e6634eb9001b6e850cc45941fdb5f389fbd518b6
Jeremy Gu <isdance2004@hotmail.com>
2016-08-27 12:09:38 +1000

    Add chap 02-03

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,22 @@
 import {Component} from '@angular/core';
 import {Hero} from './hero';
+import {Heroes} from './hero.service';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/tmpl/app.component.html'
+  templateUrl: 'app/tmpl/app.component.html',
+  styleUrls: ['app/css/app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero:Hero = {
-    id:1,
-    name : 'Windstorm'
+  // hero:Hero = {
+  //   id:1,
+  //   name : 'Windstorm'
+  // }
+  heroes: Hero[] = Heroes.HEROES;
+
+  constructor() {
+    //this.heroes = Heroes.HEROES;
+    console.log(this.heroes);
   }
 }
